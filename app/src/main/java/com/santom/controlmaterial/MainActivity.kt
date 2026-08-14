b2736d13f73127280444017faa76f70f8e297814
CraquerPianist2 <eldiosrompenokias@gmail.com>
2026-08-14 23:51:37 +0200

    Initial commit: Control de material base

---
+++ app/src/main/java/com/santom/controlmaterial/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.santom.controlmaterial
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+
+        // Configuración para permitir JavaScript y almacenamiento local persistente
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowFileAccess = true
+
+        webView.webViewClient = WebViewClient()
+
+        // Cargar el archivo index.html que está en la carpeta assets
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
