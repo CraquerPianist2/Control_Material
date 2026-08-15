ceacdb62659979ca2aec7602e6752ce02f4f8033
CraquerPianist2 <eldiosrompenokias@gmail.com>
2026-08-15 16:04:09 +0200

    Limpiar claves y subir index.html definitivo

--- app/src/main/java/com/santom/controlmaterial/MainActivity.kt
+++ app/src/main/java/com/santom/controlmaterial/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.santom.controlmaterial
 
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Ocultar las barras del sistema
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContentView(R.layout.activity_main)
 
         val webView = findViewById<WebView>(R.id.webView)
 
-        // Configuración para permitir JavaScript y almacenamiento local persistente
         val webSettings: WebSettings = webView.settings
+
+        @Suppress("SetJavaScriptEnabled")
         webSettings.javaScriptEnabled = true
+
         webSettings.domStorageEnabled = true
         webSettings.allowFileAccess = true
         webSettings.useWideViewPort = true
         webSettings.loadWithOverviewMode = true
 
+        webView.webChromeClient = WebChromeClient()
         webView.webViewClient = WebViewClient()
-
-        // Cargar el archivo index.html que está en la carpeta assets
         webView.loadUrl("file:///android_asset/index.html")
     }
 }
